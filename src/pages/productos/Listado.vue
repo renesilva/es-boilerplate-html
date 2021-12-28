99d925a57ce29a64989c8153236d4f5dc200fd61
Rene Silva <rene.sil@gmail.com>
2021-12-28 17:58:46 -0400

    Eliminar productos

--- src/pages/productos/Listado.vue
+++ src/pages/productos/Listado.vue
@@ -22,30 +22,47 @@
       <tbody>
       <tr v-for="(producto, index) in productos" :key="index">
         <td>
-          {{producto.product_name}}
+          {{ producto.product_name }}
         </td>
         <td>
-          {{producto.product_description}}
+          {{ producto.product_description }}
         </td>
         <td>
           <img :src="producto.product_image" class="img-fluid"/>
         </td>
         <td>
-          {{producto.product_price}}
+          {{ producto.product_price }}
         </td>
         <td class="fecha">
-          {{formatoDeFecha(producto.created_at)}}
+          {{ formatoDeFecha(producto.created_at) }}
         </td>
         <td>
           <router-link :to="'/productos/editar/' + producto.id" class="btn btn-outline-primary btn-sm">
             Editar
           </router-link>
           &nbsp;
-          <a @click="eliminarProducto(producto.id)" class="btn btn-outline-primary btn-sm">Eliminar</a>
+          <a @click="eliminarProductoModal(producto.id)" class="btn btn-outline-primary btn-sm">Eliminar</a>
         </td>
       </tr>
       </tbody>
     </table>
+    <div class="modal fade" id="deleteModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Eliminar producto</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+          </div>
+          <div class="modal-body">
+            ¿Estás seguro de eliminar este producto?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+            <button @click="eliminarProducto" type="button" class="btn btn-primary">Eliminar</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -54,23 +71,39 @@ export default {
   data() {
     return {
       productos: [],
+      modal: null,
+      productoEliminar: 0
     };
   },
   methods: {
     formatoDeFecha(fecha) {
       return moment(fecha).format('MMMM D, YYYY');
     },
-    eliminarProducto(id) {
-      console.log(id);
+    eliminarProductoModal(id) {
+      this.modal.show();
+      this.productoEliminar = id;
+    },
+    eliminarProducto(){
+      window.api
+          .delete('products/delete/' + this.productoEliminar)
+          .then((response) => {
+            this.cargarProductos();
+            this.modal.hide()
+          });
+
+    },
+    cargarProductos(){
+      window.api
+          .get('products/myProducts')
+          .then((response) => this.productos = response.data.products);
     }
   },
   computed: {},
   watch: {},
   created() {},
   mounted() {
-    window.api
-        .get('products/myProducts')
-        .then((response) => this.productos = response.data.products);
+    this.modal = new bootstrap.Modal(document.getElementById('deleteModal'));
+    this.cargarProductos();
   },
   components: {},
   // Se pueden utilizar estos hooks para el ciclo de vida
@@ -80,11 +113,12 @@ export default {
 </script>
 
 <style scoped>
-img{
-  max-width:100px;
-  height:auto;
+img {
+  max-width: 100px;
+  height: auto;
 }
-.fecha{
+
+.fecha {
   text-transform: capitalize;
 }
 </style>
